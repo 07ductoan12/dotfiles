cc531a5cb383f822bfa94b19272d11875568a40e
07ductoan12 <07ductoan12@gmail.com>
2023-10-30 09:10:52 +0700

    update notify

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,36 +1,10 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-	},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+  },
   lazy = false,
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		{
-			"rcarriga/nvim-notify",
-			config = function()
-				require("notify").setup({
-    background_colour = "#000000",
-    fps = 30,
-    icons = {
-        DEBUG = "",
-        ERROR = "",
-        INFO = "",
-        TRACE = "✎",
-        WARN = ""
-    },
-    level = 2,
-    minimum_width = 50,
-    render = "default",
-    stages = "fade_in_slide_out",
-    timeout = 5000,
-    top_down = true,
-    on_open = function(win)
-        vim.api.nvim_win_set_option(win, "winblend", 30)
-        vim.api.nvim_win_set_config(win, { zindex = 100 })
-    end,
-})
-			end,
-		},
-	},
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
 }
